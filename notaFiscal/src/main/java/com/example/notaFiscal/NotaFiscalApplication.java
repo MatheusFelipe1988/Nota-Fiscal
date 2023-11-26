3e6bf91031d7b6794e5301472a9d61c7c9d369e4
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2023-11-26 17:19:22 -0400

    Pacote Utilizado
    aqui fica arquivos que foram configurados para mensageria

---
+++ notaFiscal/src/main/java/com/example/notaFiscal/NotaFiscalApplication.java
@@ -0,0 +1,13 @@
+package com.example.notaFiscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaFiscalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaFiscalApplication.class, args);
+	}
+
+}
---
+++ notaFiscal/src/test/java/com/example/notaFiscal/NotaFiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notaFiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaFiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
